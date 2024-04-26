3d36e1b5a2a66c98af3b3f48b25cf0b89cab92d8
apoorav2910 <91264966+apoorav2910@users.noreply.github.com>
2024-04-26 10:13:45 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 print("Apoorav Kumar")
 print("21BCS2182")
-a = int(input("Enter 1st Number: "))
-b = int(input("Enter 2nd Number: "))
-print("Sum is: ", a+b)
+a = 10
+b = 25
+print("Sum of ", a, "and ", b, "is: ", a+b)
